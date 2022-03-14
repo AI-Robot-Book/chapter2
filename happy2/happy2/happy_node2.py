5ae7790ceb5eea9e7e79f639c2da427397c2c73b
demu <tenkose@hotmail.com>
2022-03-15 00:50:00 +0900

    Add files via upload

---
+++ happy2/happy2/happy_node2.py
@@ -0,0 +1,23 @@
+import rclpy # ROS2 の Python モジュールをインポートする
+from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
+import time # スリープに使用する time モジュールのインポート
+
+class HappyNode2(Node): # HappyNode2 クラス．基底クラス Node を継承した派生クラス．
+    def __init__(self): # コンストラクタ
+        super().__init__('happy_node2') # 基底クラスのコンストラクタをよび出し
+        self.print_hello() # print_hello メソッドのよび出し
+
+    def print_hello(self): # メソッド
+        while True: # 無限ループ
+            print("happy world2") # 端末に出力
+            time.sleep(1) # 1 秒間スリープ
+
+def main(): # main 関数
+    rclpy.init() # 初期化
+    node = HappyNode2() # ノードの生成
+    rclpy.spin(node) # ノードの実行
+    node.destroy_node() # ノードの破壊
+    rclpy.shutdown() # 終了処理
+
+if __name__ == '__main__': # このコードをモジュールとして import 可能にする
+    main() # main 関数．このコードのエントリーポイント（開始点）
