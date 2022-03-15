692d2ee779cd0b916b499ab13c9eef8461041e1d
demuko <tenkose@hotmail.com>
2022-03-15 23:02:07 +0900

    Fix using Flake8

--- happy/happy/happy_node.py
+++ happy/happy/happy_node.py
@@ -1,13 +1,13 @@
-import rclpy # ROS2 の Python モジュールをインポートする
-from rclpy.node import Node # rclpy.node モジュールから Node クラスをインポート
+import rclpy  # 1. ROS2 の Python モジュールをインポートする
+from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
-def main(): # main 関数
-    rclpy.init() # 初期化
-    node = Node('hello_node') # ノードの生成 
 
-    while rclpy.ok(): # CTRL+Cキーが押されるまで無限ループ
-        print("hello world") # 端末に出力
-
-    rclpy.spin(node) # ノードの実行
-    node.destroy_node() # ノードの破壊
-    rclpy.shutdown() # 終了処理
+def main():  # main 関数
+    rclpy.init()               # 2. 初期化
+    node = Node('happy_node')  # 3. ノードの生成
+    while rclpy.ok():          # CTRL+Cキーが押されるまで無限ループ
+        print("happy world")   # 端末に出力
+    rclpy.spin(node)           # 4. ノードの実行
+    node.destroy_node()        # 5. ノードの破壊
+    rclpy.shutdown()           # 6. 終了処理
+    
