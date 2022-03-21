725ae59e4f52584768eb73816f29b5d442605f03
demu <tenkose@hotmail.com>
2022-03-22 08:26:10 +0900

    コメントの追加と日本語化

--- happy/happy/happy_node3.py
+++ happy/happy/happy_node3.py
@@ -1,23 +1,23 @@
-import rclpy  # 1. ROS2 の Python モジュールをインポートする
+import rclpy  # 1. ROS2 Python モジュールのインポート
 from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
 
-class HappyNode(Node):
-    def __init__(self):
-        super().__init__('happy_node2')
-        self.timer = self.create_timer(1.0, self.timer_callback)
+class HappyNode(Node):   # HappyNodeクラス
+    def __init__(self):  # コンストラクタ
+        super().__init__('happy_node2')  # 基底クラスコンストラクタのよび出し
+        self.timer = self.create_timer(1.0, self.timer_callback) # タイマーの生成
 
-    def timer_callback(self):
-        self.get_logger().info("happy world2")
+    def timer_callback(self): # タイマーのコールバック関数
+        self.get_logger().info("ハッピーワールド３")
 
 
 def main():  # main 関数
-    print("Program start")
+    print("プログラム開始")
     rclpy.init()               # 2. 初期化
     node = HappyNode()         # 3. ノードの生成
-    print("Create a node")
-    rclpy.spin_once(node)
+    print("ノードの生成")
+    rclpy.spin_once(node)      # 4. ノードの処理．コールバック関数のよび出し
     node.destroy_node()        # 5. ノードの破壊
-    print("Destory the node")
+    print("ノードの破壊")
     rclpy.shutdown()           # 6. 終了処理
-    print("Program end")
+    print("プログラム終了")
