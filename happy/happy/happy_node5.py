7d3e1b4df6308897ac2c6e47127eec95873be967
demu <tenkose@hotmail.com>
2022-03-22 09:10:22 +0900

    日本語化

--- happy/happy/happy_node5.py
+++ happy/happy/happy_node5.py
@@ -1,27 +1,29 @@
-import rclpy  # 1. ROS2 の Python モジュールをインポートする
+import rclpy  # 1. ROS2 Python モジュールのインポート
 from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
 
-class HappyNode(Node):
-    def __init__(self):
-        super().__init__('happy_node2')
-        self.timer = self.create_timer(1.0, self.timer_callback)
+class HappyNode5(Node):  # HappyNode5クラス
+    def __init__(self):  # コンストラクタ
+        super().__init__('happy_node5')  # 基底クラスコンストラクタのよび出し
+        self.timer = self.create_timer(1.0, self.timer_callback)  # タイマーの生成
 
-    def timer_callback(self):
-        self.get_logger().info("happy world2")
+    def timer_callback(self):  # コールバック関数
+        self.get_logger().info("ハッピーワールド５") # 端末に表示
 
 
 def main():  # main 関数
-    print("Program start")
-    rclpy.init()               # 2. 初期化
-    node = HappyNode()         # 3. ノードの生成
-    print("Create a node")
-    try:
-        rclpy.spin(node)
+    print("プログラム開始")
+    rclpy.init()                # 2. 初期化
+    node = HappyNode5()         # 3. ノードの生成
+    print("ノードの生成")
+    try:                        # 例外処理．美しく終わるため．
+        # 4. ノードの処理．コールバック関数を繰り返しよび出す．
+        #    Ctrl+Cが押されるまでプログラムはここでブロックされる．
+        rclpy.spin(node)        
     except KeyboardInterrupt:
-        print("Ctrl+C pressed")
-    finally:
+        print("Ctrl+Cが押されました．")
+    finally:                       # 例外処理の終了時に常に行う処理
         node.destroy_node()        # 5. ノードの破壊
-        print("Destory the node")
+        print("ノードの破壊")
         rclpy.shutdown()           # 6. 終了処理
-        print("Program end")
+        print("プログラム終了")
