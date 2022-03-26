1041b0242ae5e3520e052a02bf421235a6287fea
demuko <tenkose@hotmail.com>
2022-03-26 12:15:32 +0900

    comment

--- my_teleop/my_teleop/my_subscriber_node.py
+++ my_teleop/my_teleop/my_subscriber_node.py
@@ -5,23 +5,26 @@ from geometry_msgs.msg import Twist
 
 # 速度指令値のトピック cmd_vel をサブスクライブして端末に表示するたけの簡単なクラス
 class MySubscriber(Node):
-    # コンストラクタ
-    def __init__(self):
+    def __init__(self):     # コンストラクタ
         super().__init__('my_subscriber_node')
 
         # サブスクライバの生成
         self.subscription = self.create_subscription(Twist, 'cmd_vel',
                                                      self.callback, 10)
 
-    # コールバック関数．端末に並進と角速度を表示する
-    def callback(self, Twist):
+    def callback(self, Twist):  # コールバック関数．端末に並進と角速度を表示する
         self.get_logger().info("Velocity: Linear=%f angular=%f"
                                % (Twist.linear.x, Twist.angular.z))
 
 
-def main(args=None):
-    rclpy.init(args=args)
-    my_subscriber = MySubscriber()
-    rclpy.spin(my_subscriber)
-    my_subscriber.destory_node()
+def main(args=None):  # main関数
+    rclpy.init()
+    node = MySubscriber()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Ctrl+CLが押されました．")
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
     rclpy.shutdown()
