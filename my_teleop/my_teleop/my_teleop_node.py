5a91bbc0f50a51217cb51ad8989b03df33c0f56f
demuko <tenkose@hotmail.com>
2022-03-26 11:57:00 +0900

    comment

--- my_teleop/my_teleop/my_teleop_node.py
+++ my_teleop/my_teleop/my_teleop_node.py
@@ -1,31 +1,20 @@
-import rclpy                         # ROS2 の Python モジュールをインポート
-from rclpy.node import Node          # rclpy.node モジュールから Node クラスをインポート
+import rclpy                        
+from rclpy.node import Node          
 from geometry_msgs.msg import Twist  # Twist メッセージ型をインポート
 
 
-# キー操作により速度指令値をパブリッシュするクラス
-class TeleopPublisher(Node):
-    # コンストラクタ
-    def __init__(self):
-        # 基底クラスコンストラクタのよび出し．ノードの生成．
-        super().__init__('teleop_publisher_node')
-
-        # パブリッシャの生成
+class TeleopPublisher(Node):  # キー操作により速度指令値をパブリッシュするクラス
+    def __init__(self):   # コンストラクタ
+        super().__init__('teleop_publisher_node')        
         self.publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-
-        # タイマーの生成
         self.timer = self.create_timer(0.01, self.timer_callback)
-
-        # Twist メッセージ型インスタンスの生成
-        self.vel = Twist()
+        self.vel = Twist()  # Twist メッセージ型インスタンスの生成
         self.vel.linear.x = 0.0
         self.vel.angular.z = 0.0
         print("Input f, b, r, l key, then press Enter key.")
 
-    # タイマーのコールバック関数
-    def timer_callback(self):
-        # キーの取得
-        key = input("f:forward, b:backward, r:right, l:left, s:stop <<")
+    def timer_callback(self):  # タイマーのコールバック関数
+        key = input("f:forward, b:backward, r:right, l:left, s:stop <<")  # キーの取得
 
         # キーの値により並進速度や角速度を変更
         if key == 'f':
@@ -42,17 +31,19 @@ class TeleopPublisher(Node):
         else:
             print("Input f, b, r, l : ")
 
-        # 速度指令メッセージのパブリッシュ
-        self.publisher.publish(self.vel)
-
-        # 端末に速度表示
+        self.publisher.publish(self.vel)  # 速度指令メッセージのパブリッシュ
         self.get_logger().info("Velocity: Linear=%f angular=%f"
                                % (self.vel.linear.x, self.vel.angular.z))
 
 
-def main(args=None):
-    rclpy.init(args=args)                 # rclpy モジュールの初期化
-    teleop_publisher = TeleopPublisher()  # インスタンスの生成
-    rclpy.spin(teleop_publisher)          # ノードの実行
-    teleop_publisher.destroy_node()       # ノードの破壊
-    rclpy.shutdown()                      # rclpy モジュールの終了処理
+def main(args=None):  # main関数
+    rclpy.init()
+    node = TeleopPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        print("Ctrl+CLが押されました．")
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+    rclpy.shutdown()
