081ee007bd6924a5eba4bee4f6d42053c34eb6d4
demu <tenkose@hotmail.com>
2022-03-29 21:43:39 +0900

    Update and rename happy_node2.py to happy_node.py

--- happy/happy/happy_node.py
+++ happy/happy/happy_node.py
@@ -2,16 +2,16 @@ import rclpy  # 1. ROS2 Python モジュールのインポート
 from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
 
-class HappyNode2(Node):  # HappyNode2クラス
+class HappyNode(Node):  # HappyNode2クラス
     def __init__(self):
         print('ノードの生成')
-        super().__init__('happy_node2')  # 基底クラスコンストラクタのよび出し
-        self.get_logger().info('ハッピーワールド２')  # ノードの処理
+        super().__init__('happy_node')  # 基底クラスコンストラクタのよび出し
+        self.get_logger().info('ハッピーワールド')  # ノードの処理
 
 
 def main():  # main 関数
     print('プログラム開始')
     rclpy.init()               # 2. 初期化
-    node = HappyNode2()        # 3. ノードの生成
+    node = HappyNode()        # 3. ノードの生成
     rclpy.shutdown()           # 5. 終了処理
     print('プログラム終了')
