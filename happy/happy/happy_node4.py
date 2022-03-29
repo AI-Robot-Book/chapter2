b3b81330f6be35fe87402f3cabf8cc41dbafa008
demu <tenkose@hotmail.com>
2022-03-29 21:23:20 +0900

    Update happy_node4.py

--- happy/happy/happy_node4.py
+++ happy/happy/happy_node4.py
@@ -2,7 +2,7 @@ import rclpy  # 1. ROS2 Python モジュールのインポート
 from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
 
-class HappyNode4(Node):  # HappyNode4クラス
+class HappyNode3(Node):  # HappyNode4クラス
     def __init__(self):  # コンストラクタ
         print('ノードの生成')
         super().__init__('happy_node4')  # 基底クラスコンストラクタのよび出し
@@ -15,9 +15,7 @@ class HappyNode4(Node):  # HappyNode4クラス
 def main():  # main 関数
     print('プログラム開始')
     rclpy.init()               # 2. 初期化
-    node = HappyNode4()        # 3. ノードの生成
+    node = HappyNode3()        # 3. ノードの生成
     rclpy.spin(node)           # 4. ノードの処理．コールバック関数を繰り返しよび出す．
-    print('ノードの破壊')
-    node.destroy_node()        # 5. ノードの破壊
-    rclpy.shutdown()           # 6. 終了処理
+    rclpy.shutdown()           # 5. 終了処理
     print('プログラム終了')
