19b2253b6b813609a5e517f4705294fe2a096158
demu <tenkose@hotmail.com>
2022-03-29 21:45:49 +0900

    Update happy_node.py

--- happy/happy/happy_node.py
+++ happy/happy/happy_node.py
@@ -6,12 +6,12 @@ class HappyNode(Node):  # HappyNodeクラス
     def __init__(self):
         print('ノードの生成')
         super().__init__('happy_node')  # 基底クラスコンストラクタのよび出し
-        self.get_logger().info('ハッピーワールド')  # ノードの処理
+        self.get_logger().info('ハッピーワールド')  # 4. ノードの処理
 
 
 def main():  # main 関数
     print('プログラム開始')
     rclpy.init()               # 2. 初期化
-    node = HappyNode()        # 3. ノードの生成
+    node = HappyNode()         # 3. ノードの生成
     rclpy.shutdown()           # 5. 終了処理
     print('プログラム終了')
