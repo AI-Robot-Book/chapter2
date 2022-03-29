bfc5ad854a2f3e44908a292dc8ec46b2500cd33f
demu <tenkose@hotmail.com>
2022-03-29 21:44:17 +0900

    Update and rename happy_node3.py to happy_node2.py

--- happy/happy/happy_node2.py
+++ happy/happy/happy_node2.py
@@ -2,20 +2,20 @@ import rclpy  # 1. ROS2 Python モジュールのインポート
 from rclpy.node import Node  # rclpy.node モジュールから Node クラスをインポート
 
 
-class HappyNode3(Node):  # HappyNode4クラス
+class HappyNode2(Node):  # HappyNode4クラス
     def __init__(self):  # コンストラクタ
         print('ノードの生成')
-        super().__init__('happy_node4')  # 基底クラスコンストラクタのよび出し
+        super().__init__('happy_node2')  # 基底クラスコンストラクタのよび出し
         self.timer = self.create_timer(1.0, self.timer_callback)  # タイマーの生成
 
     def timer_callback(self):  # タイマーのコールバック関数
-        self.get_logger().info('ハッピーワールド３')
+        self.get_logger().info('ハッピーワールド２')
 
 
 def main():  # main 関数
     print('プログラム開始')
     rclpy.init()               # 2. 初期化
-    node = HappyNode3()        # 3. ノードの生成
+    node = HappyNode2()        # 3. ノードの生成
     rclpy.spin(node)           # 4. ノードの処理．コールバック関数を繰り返しよび出す．
     rclpy.shutdown()           # 5. 終了処理
     print('プログラム終了')
