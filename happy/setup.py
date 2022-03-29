3a89270f8e2d22a13b0ef69539db31e4d1e5b154
demu <tenkose@hotmail.com>
2022-03-29 21:59:06 +0900

    Update setup.py

--- happy/setup.py
+++ happy/setup.py
@@ -23,8 +23,6 @@ setup(
             'happy_node = happy.happy_node:main',
             'happy_node2 = happy.happy_node2:main',
             'happy_node3 = happy.happy_node3:main',
-            'happy_node4 = happy.happy_node4:main',
-            'happy_node5 = happy.happy_node5:main',
-        ],
+         ],
     },
 )
