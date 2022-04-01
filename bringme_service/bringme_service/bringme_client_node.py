95a3142fca1058461f8285f16f75f1558d48d084
demu <tenkose@hotmail.com>
2022-04-02 00:31:33 +0900

    Update bringme_client_node.py

--- bringme_service/bringme_service/bringme_client_node.py
+++ bringme_service/bringme_service/bringme_client_node.py
@@ -6,19 +6,15 @@ from airobot_interfaces.srv import StringCommand
 class BringmeClient(Node):
     def __init__(self):
         super().__init__('bringme_client_node')
-        # クライアントの生成
-        self.client = self.create_client(StringCommand, 'command')
+        self.client = self.create_client(StringCommand, 'command') # クライアントの生成
         # サービスが利用できるまで待機
         while not self.client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('サービスは利用できません．待機中...')
-        # リクエストのインスタンス生成
-        self.request = StringCommand.Request()
+            self.get_logger().info('サービスは利用できません．待機中...')        
+        self.request = StringCommand.Request()  # リクエストのインスタンス生成
 
     def send_request(self, order):
-        # リクエストに値の代入
-        self.request.command = order
-        # サービスのリクエスト
-        self.future = self.client.call_async(self.request)
+        self.request.command = order  # リクエストに値の代入   
+        self.future = self.client.call_async(self.request) # サービスのリクエスト
 
 
 def main(args=None):
@@ -29,16 +25,13 @@ def main(args=None):
 
     while rclpy.ok():
         rclpy.spin_once(node)
-
         if node.future.done():  # サービスの処理が終了したら
             try:
                 response = node.future.result()  # サービスの結果をレスポンスに代入                  
             except Exception as e:
                 node.get_logger().info(f"サービスのよび出しは失敗しました．{e}")
-            else:
-                # 結果の表示
-                node.get_logger().info(
+            else:                
+                node.get_logger().info( # 結果の表示
                     f"\nリクエスト:{node.request.command} -> レスポンス: {response.answer}")
-                break
-  
+                break  
     rclpy.shutdown()
