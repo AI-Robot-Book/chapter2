a9e2467def8a1b5771cc01cc8b566464c494f61f
demu <tenkose@hotmail.com>
2022-04-02 17:34:33 +0900

    Delete happy_teleop directory

--- happy_teleop/README.md
+++
@@ -1,2 +0,0 @@
-# happy_teleop
-２章のサンプルコード teleopの自作版
--- happy_teleop/launch/happy_teleop.launch.py
+++
@@ -1,9 +0,0 @@
-import launch
-import launch_ros.actions
-
-def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            prefix='xterm -e', package='happy_teleop', executable='happy_teleop_node', output='screen')
-            ])
-
